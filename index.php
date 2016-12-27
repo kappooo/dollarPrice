dd1d0bf6dddeda7191ab3e8c5a0797a0549e6232
Ahmed Mostafa <Ahmed Mostafa>
2016-12-27 19:30:39 +0200

    function crawl http://www.xe.com/ and get latest price of currency you need based to any currency you need , just pass the code of currency to parametrs

---
+++ index.php
@@ -0,0 +1,38 @@
+﻿<?php
+function getDollarPrice($param1='USD',$param2='EGP')
+{
+
+
+
+    $pram1=strtoupper($param1);
+
+    $pram2=strtoupper($param2);
+
+
+
+	$url="http://www.xe.com/ar/currencyconverter/convert/?Amount=1&From=$param1&To=$param2";
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    $data = curl_exec($curl);
+    libxml_use_internal_errors(true);
+    $dom = new \DomDocument();
+    $dom->loadHTML($data);
+    $classname="data-amount";
+    $span=$dom->getElementsByTagName('span');
+    $ret=[];
+    foreach ($span as $item){
+        if($item->getAttribute($classname))
+            $ret[$classname]= floatval($item->getAttribute ($classname));
+    }
+    curl_close($curl);
+
+    if(isset($ret['data-amount']))
+    return $ret['data-amount'];
+    else
+        retrun -1;
+	
+}
+
+echo getDollarPrice('SAR','EGP');
